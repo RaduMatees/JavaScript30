9e34fe9207fa6051d74b893871e3af2365f3701c
Radu Matees <radumatees@gmail.com>
2017-12-17 12:36:13 +0200

    Finished JS Clock

---
+++ 02 - JS and CSS Clock/main.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.second-hand')
+const minutesHand = document.querySelector('.min-hand')
+const hoursHand = document.querySelector('.hour-hand')
+
+function setDate() {
+  // seconds
+  const now = new Date()
+  const seconds = now.getSeconds()
+  const secondsDegrees = ((seconds / 60) * 360) + 90
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+  // minutes
+  const minutes = now.getMinutes()
+  const minutesDegrees = ((minutes / 60) * 360) + 90
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`
+
+  // hours
+  const hours = now.getHours()
+  const hoursDegrees = ((hours / 12) * 360) + 90
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`
+}
+
+setInterval(setDate, 1000)
